b7f63ca4d1339f31b59e28bf73ad7085f99a4e43
Salim Ahmedabadi <ahmedabadi.salim@gmail.com>
2021-10-17 10:57:34 -0500

    Made updates

--- learnClass.py
+++ learnClass.py
@@ -2,9 +2,11 @@ class A:
     def welcome(self):
         print("Welcome to Texas!")
 
-        def sum(a,b):
-            c=a+b
+    def sum(self,a,b):
+            c = a + b
             print("Sum of two numbers: " + str(c))
-obj=A() #object of a class
+
+
+obj = A()  # object of a class
 obj.welcome()
-obj.sum(10,10)
+obj.sum(10, 50)
