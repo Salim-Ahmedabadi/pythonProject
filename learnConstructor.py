e2da3717fd13df658518fe10f2d52d3a54c39792
Salim Ahmedabadi <ahmedabadi.salim@gmail.com>
2021-10-18 19:56:10 -0500

    Made updates

--- learnConstructor.py
+++ learnConstructor.py
@@ -1,6 +1,9 @@
 class A:
-    def __init__(self):
-        c=a+b
-        print("This s a constructor")
+
+    def _init_(a, b):
+        c = a + b
+        print("This is a constructor")
         print(c)
-obj=A(10,20)
+
+
+obj = A(10,20)
