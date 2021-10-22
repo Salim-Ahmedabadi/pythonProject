55e69e4bd3886eda81ea8d220136968ab38f2f54
Salim Ahmedabadi <ahmedabadi.salim@gmail.com>
2021-10-21 22:22:50 -0500

    Made updates

---
+++ learnFunctions.py
@@ -0,0 +1,30 @@
+def welcome():
+    print("This is my first function")  # noargument and no return type
+
+
+welcome()  # call this function
+
+
+def sum(a, b):
+    c = a + b
+    return c
+
+
+x = sum(10, 20)
+
+
+def mul(i, j):
+    k = i * j
+    print(k)
+
+
+mul(50, x)
+
+#50*(10+20
+
+def returndata():
+    a=100
+    return a
+
+x=returndata()
+print(x)
