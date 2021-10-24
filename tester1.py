1baf45fffe0770b6b7cedbe5784eb499d991a384
Salim Ahmedabadi <ahmedabadi.salim@gmail.com>
2021-10-24 18:44:45 -0500

    Made updates

---
+++ tester1.py
@@ -0,0 +1,8 @@
+class A:
+    def sum(selfself,a,b):
+        c=a+b
+        print("Sum of two numbers: " + str(c))
+
+    def mul(self,a,b):
+        c=a*b
+        print("Multiplication of two numbers: " + str(c))
