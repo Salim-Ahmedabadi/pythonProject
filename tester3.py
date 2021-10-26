92cc1d878029d0ffac71c8f4f9d750f41a7dc17e
Salim Ahmedabadi <ahmedabadi.salim@gmail.com>
2021-10-26 11:05:43 -0500

    Made updates

---
+++ tester3.py
@@ -0,0 +1,10 @@
+from tester1 import A
+class C(A):
+    def sub(selfself,a,b):
+        c=a-b
+        print("Substraction of two numbers: " + str(c))
+
+obj=C()
+obj.sub(10,5)
+obj.sum(10,5)
+obj.mul(10,5)
